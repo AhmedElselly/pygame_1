eb9f15eb8bbc2907bef29bd4557a4c99c631b683
ahmedelselly <ahmedelselly@localhost.localdomain>
2023-06-24 15:14:51 +0300

    added game data and level file

--- overworld/main.py
+++ overworld/main.py
@@ -1,10 +1,37 @@
 import pygame, sys
 from settings import *
+from overworld import Overworld
+from level import Level
+
+class Game:
+    def __init__(self):
+        self.max_level = 3
+        self.overworld = Overworld(1, self.max_level, screen, self.create_level)
+        self.level_status = 'level'
+        self.overworld_status = 'overworld'
+        self.status = self.overworld_status
+    
+    def create_level(self, current_level):
+        self.level = Level(current_level, screen, self.create_overworld)
+        self.status = self.level_status
+    
+    def create_overworld(self, current_level, new_max_level):
+        if new_max_level > self.max_level:
+            self.max_level = new_max_level
+        self.overworld = Overworld(current_level, self.max_level, screen, self.create_level)
+        self.status = self.overworld_status
+
+    def run(self):
+        if self.status == self.overworld_status:
+            self.overworld.run()
+        else:
+            self.level.run()
 
 pygame.init()
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+game = Game()
 
 while True:
     for event in pygame.event.get():
@@ -13,5 +40,7 @@ while True:
             sys.exit()
 
     screen.fill('black')
+    game.run()
+
     pygame.display.update()
     clock.tick(60)
